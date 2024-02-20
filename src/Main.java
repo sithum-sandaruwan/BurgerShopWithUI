cd09e29b67cf52ab6e3e5a5903f2df159d5d35d6
Sithum <sithumsandaruwan200123@gmail.com>
2024-02-20 15:55:28 +0530

    AddOrder Form GUI Finished.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import View.AddOrder;
 import View.HomePageForm;
-import View.ViewOrderDetailsForm;
 
 public class Main {
     public static void main(String[] args) {
-        new ViewOrderDetailsForm().setVisible(true);
+        new AddOrder().setVisible(true);
     }
 }
